1b9db7f1780b4b60c34b369ece7d78a4c19115e8
eaglorn <admin@khtc.ru>
2020-07-08 17:08:20 +1000

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <img src="~assets/logo.png" alt="ХТК Лого" width="250px" />
+  <!-- <q-page class="flex flex-center">-->
+  <q-page>
+    <!-- <img src="~assets/logo.png" alt="ХТК Лого" width="250px" /> -->
+    <q-file color="lime-11" bg-color="green" filled v-model="model" @input="fileLoad">
+      <template v-slot:prepend>
+        <q-icon name="attachment" />
+      </template>
+    </q-file>
+    <div>{{this.text}}</div>
   </q-page>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      model: null,
+      text: ""
+    };
+  },
+  methods: {
+    fileLoad() {
+      const formData = new FormData();
+      formData.append("model", this.model);
+      this.$axios({
+        method: "post",
+        url: "http://46.8.146.12:4000/api/txt/table",
+        data: formData,
+        timeout: 5000,
+        responseType: "json"
+      })
+        .then(response => {
+          this.text = response.data.text;
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
