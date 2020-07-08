3e35c54c57aa02e6b36752a4df0d8a5a489ffe09
eaglorn <admin@khtc.ru>
2020-07-08 19:41:08 +1000

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,6 +7,7 @@
         <q-icon name="attachment" />
       </template>
     </q-file>
+    <q-table title="Treats" :data="data" :columns="columns" />
     <div>{{this.text}}</div>
   </q-page>
 </template>
@@ -16,7 +17,23 @@ export default {
   data() {
     return {
       model: null,
-      text: ""
+      text: "",
+      columns: [
+        { name: "name1", label: "name1", field: "name1", align: "left" },
+        { name: "name2", label: "name2", field: "name2", align: "left" },
+        { name: "name3", label: "name3", field: "name3", align: "left" },
+        { name: "name4", label: "name4", field: "name4", align: "left" },
+        { name: "name5", label: "name5", field: "name5", align: "left" },
+        { name: "name6", label: "name6", field: "name6", align: "left" },
+        { name: "name7", label: "name7", field: "name7", align: "left" },
+        { name: "name8", label: "name8", field: "name8", align: "left" },
+        { name: "name9", label: "name9", field: "name9", align: "left" },
+        { name: "name10", label: "name10", field: "name10", align: "left" },
+        { name: "name11", label: "name11", field: "name11", align: "left" },
+        { name: "name12", label: "name12", field: "name12", align: "left" },
+        { name: "name13", label: "name13", field: "name13", align: "left" }
+      ],
+      data: []
     };
   },
   methods: {
@@ -31,7 +48,30 @@ export default {
         responseType: "json"
       })
         .then(response => {
-          this.text = response.data.text;
+          var valueRows = response.data.text.split(
+            "\r\n",
+            response.data.text.split("\r\n").length - 2
+          );
+          var dataResponse = [];
+          valueRows.forEach(function(row, i, rows) {
+            var item = {};
+            var valueCell = row.split(";");
+            item.name1 = valueCell[0];
+            item.name2 = valueCell[1];
+            item.name3 = valueCell[2];
+            item.name4 = valueCell[3];
+            item.name5 = valueCell[4];
+            item.name6 = valueCell[5];
+            item.name7 = valueCell[6];
+            item.name8 = valueCell[7];
+            item.name9 = valueCell[8];
+            item.name10 = valueCell[9];
+            item.name11 = valueCell[10];
+            item.name12 = valueCell[11];
+            item.name13 = valueCell[12];
+            dataResponse.push(item);
+          });
+          this.data = dataResponse;
         })
         .catch(function(err) {
           console.log(err);
